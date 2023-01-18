46ddf351089eada0f5ba84b35e3bf744d78a230f
rowlandwilliams <rowland.williams.94@gmail.com>
2023-01-18 15:58:57 +0000

    remove h-screen and add margin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,23 @@
 <script>
-	import Skills from '../component/Skills/Skills.svelte';
 	import Bio from '../component/Bio/Bio.svelte';
+	import Education from '../component/Education/Education.svelte';
 	import Experience from '../component/Experience/Experience.svelte';
 	import Header from '../component/Header/Header.svelte';
-	import Education  from '../component/Education/Education.svelte';
+	import Skills from '../component/Skills/Skills.svelte';
 </script>
 
-<div class="grid h-screen place-items-center">
-<section class="container flex flex-col items-center border px-4 text-sm w-full md:w-[a4WidthInPixels] md:h-[a4WidthInPixels]">
-	<div class="h-full w-full overflow-hidden bg-whit">
-		<Header />
-		<Bio />
-		<Experience />
-		<div class="xxs:grid grid-rows gap-y-4 lg:grid-cols-2 gap-x-4">
-			<Skills />
-			<Education />
+<div class="grid place-items-center m-12">
+	<section
+		class="container flex flex-col items-center border px-4 text-sm w-full md:w-[a4WidthInPixels] md:h-[a4WidthInPixels]"
+	>
+		<div class="h-full w-full overflow-hidden bg-whit">
+			<Header />
+			<Bio />
+			<Experience />
+			<div class="xxs:grid grid-rows gap-y-4 lg:grid-cols-2 gap-x-4">
+				<Skills />
+				<Education />
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
 </div>
