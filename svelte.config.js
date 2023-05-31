d594cd23c27c061ae4847a8746e0f29651afe46e
Harry Kelleher <HAKE2@hscic.gov.uk>
2023-05-31 18:17:01 +0100

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+export default {
+    kit: {
+        adapter: adapter({
+            // see the 'Deployment configuration' section below
+        })
+    }
 };
 
-export default config;
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
