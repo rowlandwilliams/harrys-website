ac6d9ce1287301e11189d1f211badf9f5408ad38
rowlandwilliams <rowland.williams.94@gmail.com>
2023-05-31 19:47:54 +0100

    add preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter()
 	}
