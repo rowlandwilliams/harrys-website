163b176a2ba02a0d92cede47be653cc8635d115c
rowlandwilliams <rowland.williams.94@gmail.com>
2023-05-31 19:42:44 +0100

    further adapter tweaks

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
-import sveltePreprocess from 'svelte-preprocess';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			runtime: 'nodejs18.x'
-		})
-	}
-};
 
 export default {
-	config,
-	preprocess: sveltePreprocess()
+	kit: {
+		adapter: adapter()
+	}
 };
